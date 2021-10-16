764c695aa54274ab446cddd5b1463bc930fbdbe7
Seth Axen <seth.axen@gmail.com>
2021-10-16 22:00:25 +0200

    Support alternative optimizers

--- src/Pathfinder.jl
+++ src/Pathfinder.jl
@@ -9,23 +9,28 @@ using StatsFuns
 
 export pathfinder, multipathfinder
 
+# Note: we override the default history length to be shorter and the default line search
+# to be More-Thuente, which keeps the approximate Hessian positive-definite
+const DEFAULT_OPTIMIZER = Optim.LBFGS(; m = 5, linesearch = LineSearches.MoreThuente())
+
 function pathfinder(
     logp,
     ∇logp,
-    θ₀;
+    θ₀,
+    ndraws;
     rng = Random.default_rng(),
-    J = 5,
-    L = 1_000,
-    K = 5,
-    M = 5,
+    optimizer = DEFAULT_OPTIMIZER,
+    history_length = optimizer isa Optim.LBFGS ? optimizer.m : 5,
+    iterations = 1_000,
+    ndraws_elbo = 5,
     kwargs...,
 )
-    θs, logpθs, ∇logpθs = lbfgs(logp, ∇logp, θ₀; J = J, L = L, kwargs...)
+    θs, logpθs, ∇logpθs = optimize(logp, ∇logp, θ₀, optimizer; iterations = iterations, kwargs...)
     L = length(θs) - 1
     @assert length(logpθs) == length(∇logpθs) == L + 1
-    αs, βs, γs = cov_estimate(θs, ∇logpθs; J = J)
+    αs, βs, γs = cov_estimate(θs, ∇logpθs; history_length = history_length)
     ϕ_logqϕ_λ = map(θs, ∇logpθs, αs, βs, γs) do θ, ∇logpθ, α, β, γ
-        ϕ, logqϕ = bfgs_sample(rng, θ, ∇logpθ, α, β, γ, K)
+        ϕ, logqϕ = bfgs_sample(rng, θ, ∇logpθ, α, β, γ, ndraws_elbo)
         λ = elbo(logp.(ϕ), logqϕ)
         return ϕ, logqϕ, λ
     end
@@ -42,35 +47,30 @@ end
 function multipathfinder(
     logp,
     ∇logp,
-    θ₀s;
-    R = length(θ₀s),
+    θ₀s,
+    ndraws;
+    ndraws_per_run = 5,
     rng = Random.default_rng(),
     kwargs...,
 )
     # TODO: allow to be parallelized
     res = map(θ₀s) do θ₀
-        μ, Σ, ϕ, logqϕ = pathfinder(logp, ∇logp, θ₀; rng = rng, kwargs...)
+        μ, Σ, ϕ, logqϕ = pathfinder(logp, ∇logp, θ₀, ndraws_per_run; rng = rng, kwargs...)
         logpϕ = logp.(ϕ)
         return μ, Σ, ϕ, logpϕ - logqϕ
     end
     μs, Σs, ϕs, logws = ntuple(i -> getindex.(res, i), Val(4))
     ϕsvec = reduce(vcat, ϕs)
     logwsvec = reduce(vcat, logws)
-    ϕsample = psir(rng, ϕsvec, logwsvec, R)
+    ϕsample = psir(rng, ϕsvec, logwsvec, ndraws)
     return ϕsample
 end
 
-function lbfgs(logp, ∇logp, θ₀; J = 5, L = 1_000, ϵ = 2.2e-16, kwargs...)
+function optimize(logp, ∇logp, θ₀, optimizer; kwargs...)
     f(x) = -logp(x)
     g!(y, x) = (y .= .-∇logp(x))
 
-    options = Optim.Options(;
-        store_trace = true,
-        extended_trace = true,
-        iterations = L,
-        kwargs...,
-    )
-    optimizer = Optim.LBFGS(; m = J, linesearch = LineSearches.MoreThuente())
+    options = Optim.Options(; store_trace = true, extended_trace = true, kwargs...)
     res = Optim.optimize(f, g!, θ₀, optimizer, options)
 
     θ = Optim.minimizer(res)
@@ -82,25 +82,25 @@ end
 
 elbo(logpϕ, logqϕ) = mean(logpϕ) - mean(logqϕ)
 
-function psir(rng, ϕ, log_ratios, R)
+function psir(rng, ϕ, log_ratios, ndraws)
     logw, _ = PSIS.psis(log_ratios; normalize = true)
     w = StatsBase.pweights(exp.(logw))
-    return StatsBase.sample(rng, ϕ, w, R; replace = true)
+    return StatsBase.sample(rng, ϕ, w, ndraws; replace = true)
 end
 
 # Gilbert, J.C., Lemaréchal, C. Some numerical experiments with variable-storage quasi-Newton algorithms.
 # Mathematical Programming 45, 407–435 (1989). https://doi.org/10.1007/BF01589113
-function cov_estimate(θs, ∇logpθs; J = 5, ϵ = 1e-12)
+function cov_estimate(θs, ∇logpθs; history_length = 5, ϵ = 1e-12)
     L = length(θs) - 1
     θ = θs[1]
     N = length(θ)
     s = similar(θ)
-    # S = similar(θ, N, J)
+    # S = similar(θ, N, history_length)
     S = Vector{typeof(s)}(undef, 0)
 
     ∇logpθ = ∇logpθs[1]
     y = similar(∇logpθ)
-    # Y = similar(∇logpθ, N, J)
+    # Y = similar(∇logpθ, N, history_length)
     Y = Vector{typeof(y)}(undef, 0)
 
     α, β, γ = fill!(similar(θ), true), similar(θ, N, 0), similar(θ, 0, 0)
@@ -120,7 +120,7 @@ function cov_estimate(θs, ∇logpθs; J = 5, ϵ = 1e-12)
             push!(Y, copy(y))
             m += 1
 
-            if length(S) > J
+            if length(S) > history_length
                 popfirst!(S)
                 popfirst!(Y)
             end
@@ -135,7 +135,7 @@ function cov_estimate(θs, ∇logpθs; J = 5, ϵ = 1e-12)
         end
         push!(αs, α)
 
-        J′ = length(S) # min(m, J)
+        J′ = length(S) # min(m, history_length)
         β = similar(θ, N, 2J′)
         γ = fill!(similar(θ, 2J′, 2J′), false)
         for j = 1:J′
@@ -171,7 +171,7 @@ function cov_estimate(θs, ∇logpθs; J = 5, ϵ = 1e-12)
     return αs, βs, γs
 end
 
-function bfgs_sample(rng, θ, ∇logpθ, α, β, γ, M)
+function bfgs_sample(rng, θ, ∇logpθ, α, β, γ, ndraws)
     N = length(θ)
     F = qr(β ./ sqrt.(α))
     Q = Matrix(F.Q)
@@ -180,7 +180,7 @@ function bfgs_sample(rng, θ, ∇logpθ, α, β, γ, M)
     logdetΣ = sum(log, α) + 2logdet(L)
     μ = β * (γ * (β' * ∇logpθ))
     μ .+= θ .+ α .* ∇logpθ
-    u = randn(rng, N, M)
+    u = randn(rng, N, ndraws)
     ϕ = μ .+ sqrt.(α) .* (Q * ((L - I) * (Q' * u)) .+ u)
     logqϕ = ((logdetΣ + N * log2π) .+ sum.(abs2, eachcol(u))) ./ -2
     return map(collect, eachcol(ϕ)), logqϕ
