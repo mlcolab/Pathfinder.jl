114b74e5436575b535af8e3257bd3508af1975f7
Seth Axen <seth.axen@gmail.com>
2021-10-25 20:45:49 +0200

    Test maximize_with_trace

--- src/maximize.jl
+++ src/maximize.jl
@@ -1,16 +1,13 @@
-# like Optim.optimize, but maximizes logp instead of minimizes, and returns result with
-# logp evaluations and gradients
-function maximize_with_trace(logp, ∇logp, θ₀, optimizer; kwargs...)
-    f(x) = -logp(x)
-    g!(y, x) = (y .= .-∇logp(x))
+function maximize_with_trace(f, ∇f, x₀, optimizer; kwargs...)
+    negf(x) = -f(x)
+    g!(y, x) = (y .= .-∇f(x))
 
     options = Optim.Options(; store_trace=true, extended_trace=true, kwargs...)
-    res = Optim.optimize(f, g!, θ₀, optimizer, options)
+    res = Optim.optimize(negf, g!, x₀, optimizer, options)
 
-    θ = Optim.minimizer(res)
-    θs = Optim.x_trace(res)::Vector{typeof(θ)}
-    logpθs = -Optim.f_trace(res)
-    ∇logpθs = map(tr -> -tr.metadata["g(x)"], Optim.trace(res))::typeof(θs)
+    xs = Optim.x_trace(res)::Vector{typeof(Optim.minimizer(res))}
+    fxs = -Optim.f_trace(res)
+    ∇fxs = map(tr -> -tr.metadata["g(x)"], Optim.trace(res))::typeof(xs)
 
-    return θs, logpθs, ∇logpθs
+    return xs, fxs, ∇fxs
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 
 @testset "Pathfinder.jl" begin
     include("woodbury.jl")
+    include("maximize.jl")
     include("inverse_hessian.jl")
     include("mvnormal.jl")
     include("elbo.jl")
