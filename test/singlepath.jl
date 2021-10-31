5a88a527603400b79d3099fdf232bea85f713aa0
Seth Axen <seth.axen@gmail.com>
2021-10-31 02:28:39 +0200

    Revert "Fix test"
    This reverts commit 896beffb2f4ff3ffdd80205a74d4074beb73462d.

--- test/singlepath.jl
+++ test/singlepath.jl
@@ -5,7 +5,7 @@ using Test
 
 @testset "single path pathfinder" begin
     @testset "IsoNormal" begin
-        # here pathfinder finds the exact solution on 1st iteration
+        # here pathfinder finds the exact solution after 1 iteration
         logp(x) = -sum(abs2, x) / 2
         ∇logp(x) = -x
         ndraws = 100
@@ -16,9 +16,7 @@ using Test
             @test q.μ ≈ zeros(n)
             @test q.Σ isa Pathfinder.WoodburyPDMat
             @test q.Σ ≈ I
-            # at zeroth iteration, we already have exact mean and covariance,
-            # so history is empty
-            @test size(q.Σ.B) == (n, 0)
+            @test size(q.Σ.B) == (n, 2) # history contains only 1 iteration
             @test length(ϕ) == ndraws
             @test logqϕ ≈ logpdf.(Ref(q), ϕ)
         end
