93691b46f34c854d1dd9ec1a3d39850ffc355c7e
Seth Axen <seth.axen@gmail.com>
2021-11-03 13:24:32 +0100

    Always return the number of draws requested (#20)
    * Always return the number of draws requested
    
    * Increment version number
    
    * Increase tolerance

--- test/multipath.jl
+++ test/multipath.jl
@@ -30,8 +30,8 @@ using Test
         μ_hat = mean(ϕ; dims=2)
         Σ_hat = cov(ϕ .- μ_hat; dims=2, corrected=false)
         # adapted from the MvNormal tests
-        # allow for 3x disagreement in atol, since this method is approximate
-        multiplier = 5
+        # allow for 10x disagreement in atol, since this method is approximate
+        multiplier = 10
         for i in 1:n
             atol = sqrt(Σ[i, i] / ndraws) * 8 * multiplier
             @test μ_hat[i] ≈ μ[i] atol = atol
