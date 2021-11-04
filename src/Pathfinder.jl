2d2db36c6a4b69ef4adfe43439771d60250d0e0f
Seth Axen <seth.axen@gmail.com>
2021-11-04 22:06:46 +0100

    Run formatter

--- src/Pathfinder.jl
+++ src/Pathfinder.jl
@@ -16,7 +16,9 @@ export pathfinder, multipathfinder
 # to be More-Thuente, which keeps the approximate inverse Hessian positive-definite
 const DEFAULT_HISTORY_LENGTH = 6
 const DEFAULT_LINE_SEARCH = LineSearches.MoreThuente()
-const DEFAULT_OPTIMIZER = Optim.LBFGS(; m=DEFAULT_HISTORY_LENGTH, linesearch=DEFAULT_LINE_SEARCH)
+const DEFAULT_OPTIMIZER = Optim.LBFGS(;
+    m=DEFAULT_HISTORY_LENGTH, linesearch=DEFAULT_LINE_SEARCH
+)
 
 include("woodbury.jl")
 include("maximize.jl")
