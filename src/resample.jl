48541e30ab55fd1085b15237fb67a37bd5a62933
Seth Axen <seth.axen@gmail.com>
2021-11-23 11:01:46 +0100

    Use new PSIS interface

--- src/resample.jl
+++ src/resample.jl
@@ -6,8 +6,9 @@ Draw `ndraws` samples from `x`, with replacement.
 If `log_weights` is provided, perform Pareto smoothed importance resampling.
 """
 function resample(rng, x, log_ratios, ndraws)
-    log_weights, _ = PSIS.psis(log_ratios; normalize=true)
-    weights = StatsBase.pweights(exp.(log_weights))
-    return StatsBase.sample(rng, x, weights, ndraws; replace=true)
+    result = PSIS.psis(log_ratios)
+    weights = result.weights
+    pweights = StatsBase.ProbabilityWeights(weights, one(eltype(weights)))
+    return StatsBase.sample(rng, x, pweights, ndraws; replace=true)
 end
 resample(rng, x, ndraws) = StatsBase.sample(rng, x, ndraws; replace=true)
