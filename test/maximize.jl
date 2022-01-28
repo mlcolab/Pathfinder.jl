e3763db010c23c732d071001d6a82d3a92b24d52
Seth Axen <seth.axen@gmail.com>
2022-01-28 16:32:00 +0100

    Test optimization functions

--- test/maximize.jl
+++ test/maximize.jl
@@ -1,4 +1,5 @@
 using ForwardDiff
+using GalacticOptim
 using LinearAlgebra
 using Optim
 using Pathfinder
@@ -6,17 +7,31 @@ using Test
 
 include("test_utils.jl")
 
-@testset "maximize_with_trace" begin
+@testset "optimization" begin
     n = 10
     P = inv(rand_pd_mat(Float64, n))
     μ = randn(n)
     f(x) = -dot(x - μ, P, x - μ) / 2
     ∇f(x) = ForwardDiff.gradient(f, x)
     x0 = randn(n)
+    fun = Pathfinder.build_optim_function(f, ∇f)
+    @test fun isa GalacticOptim.OptimizationFunction
+    @test fun.grad !== nothing
+    prob = Pathfinder.build_optim_problem(fun, x0)
+    @test prob isa GalacticOptim.OptimizationProblem
+    @test prob.f === fun
+    @test prob.u0 == x0
 
     @testset "$Topt" for Topt in (Optim.BFGS, Optim.LBFGS, Optim.ConjugateGradient)
         optimizer = Topt()
-        xs, fxs, ∇fxs = Pathfinder.maximize_with_trace(f, ∇f, x0, optimizer)
+        xs, fxs, ∇fxs = Pathfinder.optimize_with_trace(prob, optimizer)
+        options = Optim.Options(; store_trace=true, extended_trace=true)
+        res = Optim.optimize(
+            x -> -f(x), (y, x) -> copyto!(y, -∇f(x)), x0, optimizer, options
+        )
+        @test Optim.iterations(res) == length(xs) - 1
+        @test Optim.x_trace(res) ≈ xs
+        @test Optim.minimizer(res) ≈ xs[end]
         @test xs[1] ≈ x0
         @test xs[end] ≈ μ
         @test fxs ≈ f.(xs)
