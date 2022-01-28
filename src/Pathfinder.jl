507b39fcb6f9abdd7c5a5c1eb5ec48f1b1555705
Seth Axen <seth.axen@gmail.com>
2022-01-28 16:30:05 +0100

    Add ForwardDiff as dependency

--- src/Pathfinder.jl
+++ src/Pathfinder.jl
@@ -3,12 +3,16 @@ module Pathfinder
 using Distributions: Distributions
 using LinearAlgebra
 using Optim: Optim, LineSearches
+using GalacticOptim: GalacticOptim
 using PDMats: PDMats
 using PSIS: PSIS
 using Random
 using Statistics: Statistics
 using StatsBase: StatsBase
 using StatsFuns: log2π
+
+# ensure that ForwardDiff is conditionally loaded by GalacticOptim
+using ForwardDiff: ForwardDiff
 
 export pathfinder, multipathfinder
 
