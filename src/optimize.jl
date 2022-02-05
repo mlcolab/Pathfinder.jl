0a334b4de1c785438b04e8ed906dfa790fa4e0b8
Seth Axen <seth.axen@gmail.com>
2022-02-06 00:57:59 +0100

    Dont terminate on NaN (#26)
    * Don't terminate on NaNs
    
    * Move gradient computation out of callback
    
    * Remove DEV designation

--- src/optimize.jl
+++ src/optimize.jl
@@ -39,23 +39,19 @@ function optimize_with_trace(prob, optimizer)
         rmul!(∇fx, -1)
         return ∇fx
     end
-    # caches for the trace of x, f(x), and ∇f(x)
+    # caches for the trace of x and f(x)
     xs = typeof(u0)[]
     fxs = typeof(fun.f(u0, nothing))[]
-    ∇fxs = typeof(similar(u0))[]
     function callback(x, nfx, args...)
-        # NOTE: GalacticOptim doesn't have an interface for accessing the gradient trace,
-        # so we need to recompute it ourselves
-        # see https://github.com/SciML/GalacticOptim.jl/issues/149
-        ∇fx = ∇f(x)
-        # terminate if optimization encounters NaNs
-        (isnan(nfx) || any(isnan, x) || any(isnan, ∇fx)) && return true
         # some backends mutate x, so we must copy it
         push!(xs, copy(x))
         push!(fxs, -nfx)
-        push!(∇fxs, ∇fx)
         return false
     end
     GalacticOptim.solve(prob, optimizer; cb=callback)
+    # NOTE: GalacticOptim doesn't have an interface for accessing the gradient trace,
+    # so we need to recompute it ourselves
+    # see https://github.com/SciML/GalacticOptim.jl/issues/149
+    ∇fxs = ∇f.(xs)
     return xs, fxs, ∇fxs
 end
