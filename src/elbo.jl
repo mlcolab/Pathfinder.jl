921c2218e29e3450abf61fdf5a0c9f7ab5cbddf1
Seth Axen <seth.axen@gmail.com>
2022-02-06 22:18:32 +0100

    Ignore NaNs when maximizing ELBO

--- src/elbo.jl
+++ src/elbo.jl
@@ -2,7 +2,7 @@ function maximize_elbo(rng, logp, dists, ndraws)
     elbo_ϕ_logqϕ = map(dists) do dist
         elbo_and_samples(rng, logp, dist, ndraws)
     end
-    lopt = argmax(first.(elbo_ϕ_logqϕ))
+    lopt = _argmax_ignore_nan(first.(elbo_ϕ_logqϕ))
     return (lopt, elbo_ϕ_logqϕ[lopt]...)
 end
 
@@ -14,3 +14,10 @@ function elbo_and_samples(rng, logp, dist, ndraws)
 end
 
 elbo_from_logpdfs(logpϕ, logqϕ) = Statistics.mean(logpϕ) - Statistics.mean(logqϕ)
+
+function _argmax_ignore_nan(x)
+    imax, _ = foldl(pairs(x)) do (i1, x1), (i2, x2)
+        return (isnan(x2) || !Base.isless(x1, x2)) ? (i1, x1) : (i2, x2)
+    end
+    return imax
+end
