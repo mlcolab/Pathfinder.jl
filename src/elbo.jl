8bc0fce97c6b6011ccc40944fb9e7f26aff396ea
Seth Axen <seth.axen@gmail.com>
2022-04-26 16:40:35 +0200

    Work around type-instability

--- src/elbo.jl
+++ src/elbo.jl
@@ -1,15 +1,20 @@
 function maximize_elbo(rng, logp, dists, ndraws, executor)
-    iter = dists |> Transducers.Map() do dist
-        elbo_and_samples(rng, logp, dist, ndraws)
+    elbo_ϕ_logqϕ1 = elbo_and_samples(rng, logp, dists[begin], ndraws)
+    elbo_ϕ_logqϕ = similar(dists, typeof(elbo_ϕ_logqϕ1))
+    elbo_ϕ_logqϕ[begin] = elbo_ϕ_logqϕ1
+    iter = eachindex(dists, elbo_ϕ_logqϕ)[begin+1:end] |> Transducers.Map() do i
+        elbo_ϕ_logqϕ[i] = elbo_and_samples(rng, logp, dists[i], ndraws)
+        return nothing
     end
-    elbo_ϕ_logqϕ = Folds.collect(iter, executor)
+    Folds.collect(iter, executor)
     _, lopt = _findmax(elbo_ϕ_logqϕ |> Transducers.Map(first))
     return (lopt, elbo_ϕ_logqϕ[lopt]...)
 end
 
 function elbo_and_samples(rng, logp, dist, ndraws)
     ϕ, logqϕ = rand_and_logpdf(rng, dist, ndraws)
-    logpϕ = eachcol(ϕ) |> Transducers.Map(logp) |> Transducers.tcollect
+    logpϕ = similar(logqϕ)
+    logpϕ .= logp.(eachcol(ϕ))
     elbo = elbo_from_logpdfs(logpϕ, logqϕ)
     return elbo, ϕ, logqϕ
 end
