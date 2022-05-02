67624f4af1b50acef75c6153e90284e9e3f6a82c
Seth Axen <seth.axen@gmail.com>
2022-05-02 18:24:39 +0200

    Test more cases

--- test/integration/Turing/runtests.jl
+++ test/integration/Turing/runtests.jl
@@ -16,16 +16,23 @@ end
     y = sin.(x) .+ randn.() .* 0.2 .+ x
     X = [x x .^ 2 x .^ 3]
     model = regression_model(X, y)
+    expected_param_names = Symbol.(["α", "β[1]", "β[2]", "β[3]", "σ"])
 
     result = pathfinder(model; ndraws=1_000)
     @test result isa PathfinderResult
     @test result.input === model
     @test size(result.draws) == (5, 1_000)
     @test result.draws_transformed isa MCMCChains.Chains
+    @test result.draws_transformed.info.pathfinder_result isa PathfinderResult
+    @test sort(names(result.draws_transformed)) == expected_param_names
+    @test all(>(0), result.draws_transformed[:σ])
 
     result = multipathfinder(model, 500; nruns=5)
     @test result isa MultiPathfinderResult
     @test result.input === model
     @test size(result.draws) == (5, 500)
     @test result.draws_transformed isa MCMCChains.Chains
+    @test result.draws_transformed.info.pathfinder_result isa MultiPathfinderResult
+    @test sort(names(result.draws_transformed)) == expected_param_names
+    @test all(>(0), result.draws_transformed[:σ])
 end
