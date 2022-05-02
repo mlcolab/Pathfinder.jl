a8c7d9f4dbb0ebd68e57bac2bbe4db825bbe7517
Seth Axen <seth.axen@gmail.com>
2022-05-02 13:39:00 +0200

    Set transformed distribution and draws

--- src/integration/measuretheory.jl
+++ src/integration/measuretheory.jl
@@ -1,15 +1,46 @@
 using .MeasureTheory: MeasureTheory
 using .TransformVariables: TransformVariables
+using Accessors: Accessors
 
 function pathfinder(measure::MeasureTheory.AbstractMeasure; kwargs...)
     logp = MeasureTheoryLogDensity(measure)
-    return pathfinder(logp; dim=TransformVariables.dimension(logp.transform), kwargs...)
+    tr = logp.transform
+    dim = TransformVariables.dimension(tr)
+    result = pathfinder(logp; dim, input=measure, kwargs...)
+    fit_dist = result.fit_distribution
+    fit_measure = MeasureTheory.MvNormal(fit_dist.μ, fit_dist.Σ)
+    fit_measure_transformed, draws_transformed = _transform_measure_and_draws(
+        tr, fit_measure, result.draws
+    )
+    result_new = Accessors.@set result.fit_distribution_transformed =
+        fit_measure_transformed
+    result_new2 = Accessors.@set result_new.draws_transformed = draws_transformed
+    return result_new2
 end
 
 function multipathfinder(measure::MeasureTheory.AbstractMeasure, ndraws::Int; kwargs...)
     logp = MeasureTheoryLogDensity(measure)
-    dim = TransformVariables.dimension(logp.transform)
-    return multipathfinder(logp, ndraws; dim, kwargs...)
+    tr = logp.transform
+    dim = TransformVariables.dimension(tr)
+    result = multipathfinder(logp, ndraws; dim, kwargs...)
+    fit_dist = result.fit_distribution
+    fit_measure_components = map(fit_dist.components) do c
+        return MeasureTheory.MvNormal(c.μ, c.Σ)
+    end
+    fit_measure = MeasureTheory.SuperpositionMeasure(fit_measure_components)
+    fit_measure_transformed, draws_transformed = _transform_measure_and_draws(
+        tr, fit_measure, result.draws
+    )
+    result_new = Accessors.@set result.fit_distribution_transformed =
+        fit_measure_transformed
+    result_new2 = Accessors.@set result_new.draws_transformed = draws_transformed
+    return result_new2
+end
+
+function _transform_measure_and_draws(transform, measure, draws)
+    measure_transformed = MeasureTheory.Pushforward(transform, measure)
+    draws_transformed = TransformVariables.transform.(Ref(transform), eachcol(draws))
+    return measure_transformed, draws_transformed
 end
 
 struct MeasureTheoryLogDensity{M,T}
