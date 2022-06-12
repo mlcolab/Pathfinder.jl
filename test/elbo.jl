9aa00c5756330bd1a3724edd8f26dd27f39ae99c
Seth Axen <seth.axen@gmail.com>
2022-06-12 18:27:11 +0200

    Fix elbo tests

--- test/elbo.jl
+++ test/elbo.jl
@@ -5,7 +5,7 @@ using Test
 using Transducers
 
 @testset "ELBO estimation" begin
-    @testset "elbo_and_samples" begin
+    @testset "_compute_elbo" begin
         σ_target = 0.08
         target_dist = Normal(0, σ_target)
         logp(x) = logpdf(target_dist, x[1])
@@ -13,22 +13,22 @@ using Transducers
         rng = Random.seed!(Random.default_rng(), 42)
         @testset for σ in σs
             dist = Normal(0, σ)
-            est = @inferred Pathfinder.elbo_and_samples(rng, logp, dist, 1_000_000)
+            est = @inferred Pathfinder._compute_elbo(rng, logp, dist, 1_000_000, Val(true))
             @test est isa Pathfinder.ELBOEstimate
 
             # explicit elbo calculation
             r = σ / σ_target
             elbo_exp = (1 - r^2) / 2 + log(r)
             @test est.value ≈ elbo_exp atol = 3 * est.std_err
-            @test est.log_densities_target ≈ logp.(eachcol(est.draws))
+            @test est.log_densities_actual ≈ logp.(eachcol(est.draws))
             @test est.log_densities_fit ≈ logpdf.(dist, first.(eachcol(est.draws)))
-            @test est.log_density_ratios == est.log_densities_target - est.log_densities_fit
+            @test est.log_density_ratios == est.log_densities_actual - est.log_densities_fit
             @test est.value ≈ mean(est.log_density_ratios)
             @test est.std_err ≈ std(est.log_density_ratios) / sqrt(1_000_000)
         end
     end
 
-    @testset "maximize_elbo" begin
+    @testset "MaximumELBO" begin
         σ_target = 0.08
         target_dist = Normal(0, σ_target)
         logp(x) = logpdf(target_dist, x[1])
@@ -41,18 +41,19 @@ using Transducers
         end
         @testset "$executor" for executor in executors
             rng = Random.seed!(Random.default_rng(), 42)
-            lopt, estimates = @inferred Pathfinder.maximize_elbo(
-                rng, logp, dists, 100, executor
-            )
-            @test lopt == 3
+            opt = Pathfinder.MaximumELBO(; rng, executor, ndraws=100, save_draws=true)
+            success, dist, lopt, estimates = @inferred opt(logp, nothing, nothing, dists)
+            @test lopt == 2
             @test estimates[lopt].value ≈ 0
-            rng = Random.seed!(Random.default_rng(), 42)
-            lopt2, estimates2 = Pathfinder.maximize_elbo(rng, logp, dists, 100, executor)
+            Random.seed!(rng, 42)
+            success2, dist2, lopt2, estimates2 = @inferred opt(
+                logp, nothing, nothing, dists
+            )
             @test lopt2 == lopt
             @test getproperty.(estimates2, :value) == getproperty.(estimates, :value)
             @test getproperty.(estimates2, :std_err) == getproperty.(estimates, :std_err)
-            lopt3, estimates3 = @inferred Pathfinder.maximize_elbo(
-                rng, logp, dists[2:1], 100, executor
+            success3, dist3, lopt3, estimates3 = @inferred opt(
+                logp, nothing, nothing, dists[1:1]
             )
             @test lopt3 == 0
             @test isempty(estimates3)
