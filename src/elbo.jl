211847047c658effe8bac960b4a4c7a4469fc002
Seth Axen <seth.axen@gmail.com>
2022-06-12 15:51:04 +0200

    Implement MaximumELBO optimizer

--- src/elbo.jl
+++ src/elbo.jl
@@ -1,17 +1,35 @@
-function maximize_elbo(rng, logp, dists, ndraws, executor)
-    EE = Core.Compiler.return_type(
-        elbo_and_samples, Tuple{typeof(rng),typeof(logp),eltype(dists),Int}
-    )
-    estimates = similar(dists, EE)
-    isempty(estimates) && return 0, estimates
-    Folds.map!(estimates, dists, executor) do dist
-        return elbo_and_samples(rng, logp, dist, ndraws)
-    end
-    _, iteration_opt = _findmax(estimates |> Transducers.Map(est -> est.value))
-    return iteration_opt, estimates
+struct MaximumELBO{save_draws,R,E}
+    rng::R
+    ndraws::Int
+    executor::E
+end
+function MaximumELBO(;
+    rng::Random.AbstractRNG=Random.GLOBAL_RNG,
+    ndraws::Int=DEFAULT_NDRAWS_ELBO,
+    executor=Transducers.SequentialEx(),
+    save_draws::Bool=false,
+)
+    return MaximumELBO{save_draws,typeof(rng),typeof(executor)}(rng, ndraws, executor)
 end
 
-function elbo_and_samples(rng, logp, dist, ndraws)
+function (optimizer::MaximumELBO{R,E,save_draws})(logp, _, _, dists) where {R,E,save_draws}
+    (; rng, ndraws, executor) = optimizer
+    EE = Core.Compiler.return_type(
+        _compute_elbo, Tuple{typeof(rng),typeof(logp),eltype(dists),Int}
+    )
+    dists_not_init = @views dists[(begin + 1):end]
+    estimates = similar(dists_not_init, EE)
+    isempty(estimates) && return false, first(dists), 0, estimates
+    Folds.map!(estimates, dists_not_init, executor) do dist
+        return _compute_elbo(rng, logp, dist, ndraws)
+    end
+    _, iteration_opt = _findmax(estimates |> Transducers.Map(est -> est.value))
+    elbo_opt = estimates[iteration_opt].value
+    success = !isnan(elbo_opt) & (elbo_opt != -Inf)
+    return success, dists[iteration_opt + 1], iteration_opt, estimates
+end
+
+function _compute_elbo(rng, logp, dist, ndraws)
     ϕ, logqϕ = rand_and_logpdf(rng, dist, ndraws)
     logpϕ = similar(logqϕ)
     logpϕ .= logp.(eachcol(ϕ))
