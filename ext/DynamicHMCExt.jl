9541502c8b2e064e38b0a560e8e6fb4fbe89cd2b
Seth Axen <seth@sethaxen.com>
2023-01-02 14:15:07 +0100

    Make extensions modules

--- ext/DynamicHMCExt.jl
+++ ext/DynamicHMCExt.jl
@@ -1,11 +1,17 @@
-using .DynamicHMC: DynamicHMC
+module DynamicHMCExt
 
-function DynamicHMC.GaussianKineticEnergy(M⁻¹::WoodburyPDMat)
-    return DynamicHMC.GaussianKineticEnergy(M⁻¹, inv(pdfactorize(M⁻¹).R))
+using Pathfinder: Pathfinder
+using PDMats: PDMats
+using DynamicHMC: DynamicHMC
+
+function DynamicHMC.GaussianKineticEnergy(M⁻¹::Pathfinder.WoodburyPDMat)
+    return DynamicHMC.GaussianKineticEnergy(M⁻¹, inv(Pathfinder.pdfactorize(M⁻¹).R))
 end
 
 function DynamicHMC.kinetic_energy(
-    κ::DynamicHMC.GaussianKineticEnergy{<:WoodburyPDMat}, p, q=nothing
+    κ::DynamicHMC.GaussianKineticEnergy{<:Pathfinder.WoodburyPDMat}, p, q=nothing
 )
     return PDMats.quad(κ.M⁻¹, p) / 2
 end
+
+end  # module
