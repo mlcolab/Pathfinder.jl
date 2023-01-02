22632c124c04d8d09cf4a1898df59c796f1b735c
Seth Axen <seth@sethaxen.com>
2023-01-02 15:20:02 +0100

    Require Turing dependencies used

--- src/Pathfinder.jl
+++ src/Pathfinder.jl
@@ -53,7 +53,11 @@ function __init__()
             include("../ext/DynamicHMCExt.jl")
         end
         Requires.@require Turing = "fce5fe82-541a-59a6-adf8-730c64b5f9a0" begin
-            include("../ext/TuringExt.jl")
+            Requires.@require DynamicPPL = "366bfd00-2699-11ea-058f-f148b4cae6d8" begin
+                Requires.@require MCMCChains = "c7f686f2-ff18-58e9-bc7b-31028e88f75d" begin
+                    include("../ext/TuringExt.jl")
+                end
+            end
         end
     end
 end
