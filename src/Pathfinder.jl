1cfd012e64ec8d5e360cf424a0e91bc118b28583
Seth Axen <seth@sethaxen.com>
2023-12-05 17:17:27 +0100

    Remove outdated comment

--- src/Pathfinder.jl
+++ src/Pathfinder.jl
@@ -24,8 +24,6 @@ using UnPack: @unpack
 export PathfinderResult, MultiPathfinderResult
 export pathfinder, multipathfinder
 
-# Note: we override the default history length to be shorter and the default line search
-# to be More-Thuente, which keeps the approximate inverse Hessian positive-definite
 const DEFAULT_HISTORY_LENGTH = 6
 const DEFAULT_LINE_SEARCH = LineSearches.HagerZhang()
 const DEFAULT_LINE_SEARCH_INIT = LineSearches.InitialHagerZhang()
