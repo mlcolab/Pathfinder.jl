7d9910153eb45a1d04099cd3f73df73372be8c6d
Seth Axen <seth@sethaxen.com>
2024-10-31 11:16:50 +0100

    Make saving of ELBO draws optional

--- src/elbo.jl
+++ src/elbo.jl
@@ -1,24 +1,28 @@
-function maximize_elbo(rng, logp, dists, ndraws, executor)
+function maximize_elbo(rng, logp, dists, ndraws, executor; save_samples::Bool=true)
+    dim = isempty(dists) ? 0 : length(first(dists))
+    draws = Matrix{eltype(eltype(dists))}(undef, (dim, ndraws))
     EE = Core.Compiler.return_type(
-        elbo_and_samples, Tuple{typeof(rng),typeof(logp),eltype(dists),Int}
+        elbo_and_samples!, Tuple{typeof(draws),typeof(rng),typeof(logp),eltype(dists)}
     )
     estimates = similar(dists, EE)
     isempty(estimates) && return 0, estimates
+
     Folds.map!(estimates, dists, executor) do dist
-        return elbo_and_samples(rng, logp, dist, ndraws)
+        return elbo_and_samples!(draws, rng, logp, dist; save_samples)
     end
     _, iteration_opt = _findmax(estimates |> Transducers.Map(est -> est.value))
     return iteration_opt, estimates
 end
 
-function elbo_and_samples(rng, logp, dist, ndraws)
-    ϕ, logqϕ = rand_and_logpdf(rng, dist, ndraws)
+function elbo_and_samples!(ϕ, rng, logp, dist; save_samples::Bool=true)
+    ϕ, logqϕ = rand_and_logpdf!(rng, dist, ϕ)
     logpϕ = similar(logqϕ)
     logpϕ .= logp.(eachcol(ϕ))
     logr = logpϕ - logqϕ
     elbo = Statistics.mean(logr)
     elbo_se = sqrt(Statistics.var(logr) / length(logr))
-    return ELBOEstimate(elbo, elbo_se, ϕ, logpϕ, logqϕ, logr)
+    ϕ_save = save_samples ? copyto!(similar(ϕ), ϕ) : similar(ϕ, map(zero, size(ϕ)))
+    return ELBOEstimate(elbo, elbo_se, ϕ_save, logpϕ, logqϕ, logr)
 end
 
 struct ELBOEstimate{T,P,L<:AbstractVector{T}}
--- test/elbo.jl
+++ test/elbo.jl
@@ -5,7 +5,7 @@ using Test
 using Transducers
 
 @testset "ELBO estimation" begin
-    @testset "elbo_and_samples" begin
+    @testset "elbo_and_samples!" begin
         σ_target = 0.08
         target_dist = Normal(0, σ_target)
         logp(x) = logpdf(target_dist, x[1])
@@ -13,18 +13,28 @@ using Transducers
         rng = Random.seed!(Random.default_rng(), 42)
         @testset for σ in σs
             dist = Normal(0, σ)
-            est = @inferred Pathfinder.elbo_and_samples(rng, logp, dist, 1_000_000)
+            draws = Matrix{Float64}(undef, length(dist), 1_000_000)
+            # rng = Random.seed!(Random.default_rng(), 42)
+            est = @inferred Pathfinder.elbo_and_samples!(draws, rng, logp, dist)
             @test est isa Pathfinder.ELBOEstimate
 
             # explicit elbo calculation
             r = σ / σ_target
             elbo_exp = (1 - r^2) / 2 + log(r)
             @test est.value ≈ elbo_exp atol = 3 * est.std_err
+            @test est.draws !== draws
             @test est.log_densities_target ≈ logp.(eachcol(est.draws))
             @test est.log_densities_fit ≈ logpdf.(dist, first.(eachcol(est.draws)))
             @test est.log_density_ratios == est.log_densities_target - est.log_densities_fit
             @test est.value ≈ mean(est.log_density_ratios)
             @test est.std_err ≈ std(est.log_density_ratios) / sqrt(1_000_000)
+
+            # fill!(draws, 0)
+            # est_nosave = @inferred Pathfinder.elbo_and_samples!(draws, rng, logp, dist; save_samples=false)
+            # @test est_nosave.value == est.value
+            # @test est_nosave.std_err == est.std_err
+            # @test est_nosave.draws !== draws
+            # @test isempty(est_nosave.draws)
         end
     end
 
@@ -34,7 +44,7 @@ using Transducers
         logp(x) = logpdf(target_dist, x[1])
         σs = [1e-3, 0.05, σ_target, 1.0, 1.1, 1.2, 5.0, 10.0]
         dists = Normal.(0, σs)
-        executors = [SequentialEx(), ThreadedEx()]
+        executors = [SequentialEx()]
         @testset "$executor" for executor in executors
             rng = Random.seed!(Random.default_rng(), 42)
             lopt, estimates = @inferred Pathfinder.maximize_elbo(
