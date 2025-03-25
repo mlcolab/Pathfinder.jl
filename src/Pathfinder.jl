7119d41f072edf0509e0e5d3a4df1cb53eb9ba81
Seth Axen <seth@sethaxen.com>
2025-03-25 11:45:49 +0100

    Remove outdated check for `get_extension` added in v1.9

--- src/Pathfinder.jl
+++ src/Pathfinder.jl
@@ -53,20 +53,6 @@ function __init__()
     Requires.@require AdvancedHMC = "0bf59076-c3b1-5ca4-86bd-e02cd72cde3d" begin
         include("integration/advancedhmc.jl")
     end
-    @static if !isdefined(Base, :get_extension)
-        Requires.@require DynamicHMC = "bbc10e6e-7c05-544b-b16e-64fede858acb" begin
-            include("../ext/PathfinderDynamicHMCExt.jl")
-        end
-        Requires.@require Accessors = "7d9f7c33-5ae7-4f3b-8dc6-eff91059b697" begin
-            Requires.@require DynamicPPL = "366bfd00-2699-11ea-058f-f148b4cae6d8" begin
-                Requires.@require MCMCChains = "c7f686f2-ff18-58e9-bc7b-31028e88f75d" begin
-                    Requires.@require Turing = "fce5fe82-541a-59a6-adf8-730c64b5f9a0" begin
-                        include("../ext/PathfinderTuringExt.jl")
-                    end
-                end
-            end
-        end
-    end
 end
 
 end
